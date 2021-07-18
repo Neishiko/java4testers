31f8e85801966461f53a9f2666f6685bc1fa14ec
Neishiko <37530498+Neishiko@users.noreply.github.com>
2021-07-18 15:14:21 +0300

    homework 2

---
+++ testbox/src/main/java/home/works/java4testers/HomeWork2.java
@@ -0,0 +1,11 @@
+package home.works.java4testers;
+
+public class HomeWork2 {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(3,5);
+        Point p2 = new Point(16,25);
+
+        System.out.println("Расстояние между точками p1 и p2 = " + (p1.distance(p2)));
+    }
+}
---
+++ testbox/src/main/java/home/works/java4testers/Point.java
@@ -0,0 +1,16 @@
+package home.works.java4testers;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x1, double y1) {
+        this.x = x1;
+        this.y = y1;
+    }
+
+    public double distance(Point p2){
+        return Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
+    }
+
+}
