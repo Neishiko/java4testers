4d25627078ab11549b6d2bccab1aabc16e6286e0
Neishiko <37530498+Neishiko@users.noreply.github.com>
2021-07-25 22:58:04 +0300

    New test + TestBase class

---
+++ addressbook-web-tests/src/test/java/ru/works/addressbook/GroupDeletionTest.java
@@ -0,0 +1,15 @@
+package ru.works.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase{
+
+	@Test
+	public void testGroupDeletion() throws Exception {
+		goToGroupPage();
+		selectGroup();
+		deleteSelectedGroup();
+		returnToGroupPage();
+	}
+
+}
